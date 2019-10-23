53d1a13a20b0a2cf556c6a04e46ba7e24d24c4d7
cheetah-poc@belink.com <cheetah-poc@belink.com>
2019-10-23 11:59:59 +0800

    webpack支持箭头函数

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import '@style/login.scss';
 
 class Login extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+
     render() {
+        const { getFieldDecorator } = this.props.form;
         return (
             <div className="login">
-                <h1>122</h1>
+                <Form onSubmit={this.handleSubmit} className="login-form">
+                    <Form.Item>
+                        {getFieldDecorator('username', {
+                            rules: [{ required: true, message: 'Please input your username!' }],
+                        })(
+                            <Input
+                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                placeholder="Username"
+                            />,
+                        )}
+                    </Form.Item>
+                    <Form.Item>
+                        {getFieldDecorator('password', {
+                            rules: [{ required: true, message: 'Please input your Password!' }],
+                        })(
+                            <Input
+                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                type="password"
+                                placeholder="Password"
+                            />,
+                        )}
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                        </Button>
+                    </Form.Item>
+                </Form>
             </div>
         );
     }
 }
-
-export default Login;
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(Login);
+export default WrappedNormalLoginForm;
